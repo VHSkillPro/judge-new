5f1af847438eaf6a306f6c21c632d2ac9b1b2b1c
Hazzu <hailaconsutu558@gmail.com>
2023-06-19 19:17:48 +0700

    Update compile and run options constant

---
+++ settings/CompileOptions.py
@@ -0,0 +1,25 @@
+import Constants
+
+compileOptions = {
+    Constants.WINDOWS : {
+        "c" : "gcc -std=c11 -Wall -O2 -lm -fmax-errors=5 -march=native -s $fileName -o $fileNameWithoutExt.exe",
+        "cpp" : "g++ -std=c++17 -Wall -O2 -lm -fmax-errors=5 -march=native -s $fileName -o $fileNameWithoutExt.exe"
+    },
+    Constants.LINUX : {
+        "c" : "gcc -std=c11 -Wall -O2 -lm -fmax-errors=5 -march=native -s $fileName -o $fileNameWithoutExt",
+        "cpp" : "g++ -std=c++17 -Wall -O2 -lm -fmax-errors=5 -march=native -s $fileName -o $fileNameWithoutExt"
+    }
+}
+
+runOptions = {
+    Constants.WINDOWS : {
+        "c" : "$fileNameWithoutExt.exe",
+        "cpp" : "$fileNameWithoutExt.exe",
+        "py" : "python3 -m compileall -q $fileName"
+    },
+    Constants.LINUX : {
+        "c" : "./$fileNameWithoutExt",
+        "cpp" : "./$fileNameWithoutExt",
+        "py" : "python3 -m compileall -q $fileName"
+    }
+}
