437a19186fcba2fd60ce0ca03ad3322a79b1f9a9
Hazzu <hailaconsutu558@gmail.com>
2023-06-19 19:24:51 +0700

    update judges setting

---
+++ settings/Judges.py
@@ -0,0 +1,5 @@
+TESTCASE_NUMBER = 100
+''' Default testcase number (Số test chấm mặc định) '''
+
+TIME_LIMIT_PER_TESTCASE = 1000 # ms
+''' Maximum time for each testcase (Thời gian tối đa cho mỗi testcase) '''
